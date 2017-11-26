0e1f69d6340934073222c37de703edc18dbc439c
Tiezhu Wu <wutiezhu_tj@163.com>
2017-11-26 21:04:59 +0800

    Update test1.c

--- test1.c
+++ test1.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <malloc.h>
+
+//改变型：加一个&
+//引用型：不用加&
+typedef struct List
+{
+  float data;
+  struct List *next;
+};
+int count = 0;    //计数
+struct 
 int main(void)
 {
   
   return 0;
+}
+struct List InitList(void)
+{
+  
 }
